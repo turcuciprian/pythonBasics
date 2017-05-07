47c254a0a72cd7db24e75665f81bd00f504aff0c
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-08 00:14:19 +0300

    first file

---
+++ variables.py
@@ -0,0 +1,20 @@
+import random
+import sys
+import os
+
+print('Hello World')
+
+# comment
+
+'''
+multiline comment
+'''
+
+name = 'Cip'
+bolean = False
+bolean = True
+number = 12
+
+print (name)
+print (bolean)
+print (number)

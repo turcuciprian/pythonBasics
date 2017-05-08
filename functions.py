7c767459159497bb5c22d61b311d63094400531b
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-09 00:35:44 +0300

    concatenating part of a string with another string

--- functions.py
+++ functions.py
@@ -23,3 +23,4 @@ longstring = "I'll catch you if you fall - the floor"
 print(longstring[0:4]) # first 4 characters
 print(longstring[-5:]) # the last 5 characters from a string
 print(longstring[:-5]) # everything up to the last 5 characters
+print(longstring[:4]+' be there') # concatenating a part of a string with another string

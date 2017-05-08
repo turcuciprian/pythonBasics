9a97282e65a088d143420b093e06c748b8319360
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-09 00:11:37 +0300

    2 for's examples

--- looping.py
+++ looping.py
@@ -2,3 +2,12 @@ import sys
 import random
 import os
 
+for x in range(0,10):
+    print(x,' ',end="")
+
+print('\n')
+
+grocery_list = ['juice', 'Tomatoes','Potatoes','Bannanas']
+
+for y in grocery_list:
+    print(y)

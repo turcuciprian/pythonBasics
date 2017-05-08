1fa64d2b5a69b93255fd2f43ca40005b49cb744b
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-08 23:34:43 +0300

    using min max len on tuples

--- tuple.py
+++ tuple.py
@@ -2,6 +2,10 @@ import os
 import sys
 import random
 
-pi_tuple = (3,1,4,1,5,9)
+pi_tuple = (3,0,4,1,5,9)
 new_tuple = list(pi_tuple) #make the tuple a list
 new_list = tuple(new_tuple) # make the list a tuple
+
+print(len(pi_tuple))
+print(min(pi_tuple))
+print(max(pi_tuple))

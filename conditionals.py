4c8c7ce64d56bd4e4102e06c3980daac1ef7b5c5
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-09 00:05:49 +0300

    more elif and else with and & or

--- conditionals.py
+++ conditionals.py
@@ -8,5 +8,13 @@ if age > 16 :
     print('you can drive')
 elif age > 14:
     print ('just the states, I think')
-else
+else:
     print('you are not old enough to drive')
+
+
+if ((age >=1) and (age <=18)):
+    print('you get a birthday')
+elif (age ==21) or (age >=65):
+    print('You\'re old')
+else:
+    print("you're dead!")

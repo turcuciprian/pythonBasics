7f2a5d40e6149beb9a509a620671a5a9be8d0a23
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-09 00:50:17 +0300

    identifying numbers checking if all characters are letters finding the index position of a string inside a string capitalizing the string changing the case on the string

--- functions.py
+++ functions.py
@@ -25,3 +25,11 @@ print(longstring[-5:]) # the last 5 characters from a string
 print(longstring[:-5]) # everything up to the last 5 characters
 print(longstring[:4]+' be there') # concatenating a part of a string with another string
 print('%c is my %s letter and my number %d number is %.5f' % ('X','favorite',1,.14))
+
+print(longstring.capitalize())
+print(longstring.swapcase())
+print(longstring.find('floor'))
+print('all characters are letters')
+print(longstring.isalpha()) #all string characters are string letters - no spaces or special characters
+print('there are numbers in the string')
+print(longstring.isalnum()) # if there are only numbers in the string - nothing but numbers

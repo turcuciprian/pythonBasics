8e07f8c99d29f5fe4b0aed7a08bf0b39fb112af9
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-09 00:30:22 +0300

    commenting previous code, to play with strings

--- functions.py
+++ functions.py
@@ -2,18 +2,22 @@ import random
 import sys
 import os
 
+#
+# def addNumber(fNum, lNum):
+#     sumNum = fNum + lNum
+#
+#     return sumNum
+#
+# stringx = addNumber(1,4)
+# print(stringx)
+#
+#
+# print('what is your name?')
+#
+# name = sys.stdin.readline()
+#
+# print('hello', name)
 
-def addNumber(fNum, lNum):
-    sumNum = fNum + lNum
+longstring = "I'll catch you if you fall - the floor"
 
-    return sumNum
-
-stringx = addNumber(1,4)
-print(stringx)
-
-
-print('what is your name?')
-
-name = sys.stdin.readline()
-
-print('hello', name)
+print(longstring[0:4])

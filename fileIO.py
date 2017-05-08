604fb6e9faec97caf83f4b6d4269278ed710aa4a
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-09 01:07:43 +0300

    reading and writing from a file

--- fileIO.py
+++ fileIO.py
@@ -2,3 +2,11 @@ import sys
 import os
 import random
 
+test_file = open("test.txt",'wb')
+
+print(test_file.mode)
+
+print(test_file.name)
+
+
+test_file.write(bytes('write me to the file\n','UTF-8'))

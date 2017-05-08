425afce6f6f0f51e741934494ef410d584b29c92
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-09 00:16:24 +0300

    loop inside loop , multidimensional list

--- looping.py
+++ looping.py
@@ -11,3 +11,14 @@ grocery_list = ['juice', 'Tomatoes','Potatoes','Bannanas']
 
 for y in grocery_list:
     print(y)
+
+
+    for x in [2,4,6,8,10]:
+        print(x)
+
+
+    num_list = [[1,2,3],[10,20,30],[100,200,300]]
+
+for x in range(0,3):
+    for y in range(0,3):
+        print(num_list[x][y])

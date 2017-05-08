ba3b00566a4100ce3bd9a35e935bbacd12bce501
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-09 00:24:40 +0300

    did a basic while, showing odd numbers and stopping at 9

--- looping.py
+++ looping.py
@@ -29,3 +29,17 @@ random_num = random.randrange(0,100)
 while(random_num != 15):
     print(random_num)
     random_num = random.randrange(0,100)
+
+i =0
+while i <=20:
+    if(i%2==0):
+        print(i)
+    elif i==9:
+        break
+    else:
+        i+=1
+        continue
+        
+    i+=1
+
+

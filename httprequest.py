8dbc61dffa50d8330ef8121045a2ae591585c4f5
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-11 23:55:54 +0300

    commenting the request py

--- httprequest.py
+++ httprequest.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
-
+# //connecting to a site
 connection = urlopen('http://ciprianturcu.com')
+# reading the contents of the site
 content = connection.read()
+# printing the read content
 print(content)
